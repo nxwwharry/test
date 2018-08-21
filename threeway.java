f4233b0c7fc5dbe35fd5766e98f9d2637e55d2a9
Administrator <admin@example.com>
2018-08-21 17:11:27 +0800

    first commit

---
+++ threeway.java
@@ -0,0 +1,34 @@
+package com.junit;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+
+
+public class threeway {
+
+	
+	@Before
+	public void setUp() throws Exception {
+	}
+	@Test
+	public void wayone() {
+		//fail("Not yet implemented");
+		assertEquals(System.currentTimeMillis(), System.currentTimeMillis());
+	}
+
+	@Test
+	public void waytwo() {
+		//fail("Not yet implemented");
+		assertEquals("2", String.valueOf("2"));
+	}
+
+	@Test
+	public void waythree() {
+		//fail("Not yet implemented");
+		assertEquals("happy", "unhappy".substring(2));
+	}
+
+}
